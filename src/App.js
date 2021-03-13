ee3a29464458a5176c5117799476b42510309e9d
Azzouz-ichrak <ichrak12.azzouz@gmail.com>
2021-03-13 13:23:36 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const App = () =>(
-  <div>
-    <h1>hello world..</h1>
-  </div>
-)
+const App = () => {
+  const now = new Date();
+  console.log("NOW:---", now)
+  const a = 10;
+  const b = 20;
+  console.log('hello from component');
+  return(
+    <div>
+      <h1>hello world.. it's {now.toString()}</h1>
+      <p>{a} + {b} = {a+b}</p>
+    </div>
+  )
+}
 export default App;
