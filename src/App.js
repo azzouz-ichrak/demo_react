188d9565b48fbe15e87e2f87e1f323fcc8ddce8e
seyf <mejri.seyf@gmail.com>
2021-04-03 12:43:05 +0100

    updated project

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ const Hello = (props) =>{
   )
 }
 
-const Footer = () =>{
-  const today = new Date()
-  return(
-    <div>
-      hello app created {today.toString()}
-    </div>
-  )
-}
 
 const App = () => {
   const name = 'azzouz';
@@ -27,7 +19,6 @@ const App = () => {
       <h1>Greeting</h1>
       <Hello name="ichrak" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Footer />
 
     </div>
   )
