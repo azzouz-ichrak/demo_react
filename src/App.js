d4f6b5b090314cdee32adc0dbe248287863ccc15
chams <chamseddinechelly@gmail.com>
2021-04-10 10:50:29 +0100

    Page re-rendering

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
 
-
-const Hello = ({ name, age }) => {
-  // const name = props.name
-  // const age = props.age
-  // const { name, age } = props
-  // console.log("----props----", props)
-
-  // const bornYear = () => {
-  //   const yearNow = new Date().getFullYear()
-  //   // console.log("yearNow", yearNow)
-  //   return yearNow - props.age
-  // }
-  const bornYear = () => new Date().getFullYear() - age
+const App = (props) => {
+  const { counter } = props
 
   return (
     <div>
-      <p>hello {name}, you are {age} years old!!</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'mejri';
-  const age = 25;
-
-  return (
-    <div>
-      <h1>Greeting</h1>
-      <Hello name="seyf" age={26 + 10} />
-      <Hello name={name} age={age} />
-
+      <h3>
+        {counter}
+      </h3>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(
+    <App counter={counter} />,
+    document.getElementById('root')
+  );
+}
+
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+// counter += 1
+// refresh()
+
+setInterval (() => {
+  refresh()
+  counter += 1
+}, 1000)
