7209e404012fd8679e15307b98a0b6ea1b0fc82d
chams <chamseddinechelly@gmail.com>
2021-04-10 11:15:28 +0100

    Stateful component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 
-const App = (props) => {
-  const { counter } = props
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.render(
-    <App counter={counter} />,
-    document.getElementById('root')
-  );
-}
-
-// refresh()
-// counter += 1
-// refresh()
-// counter += 1
-// refresh()
-// counter += 1
-// refresh()
-
-setInterval (() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.render(<App />,
+  document.getElementById('root'))
