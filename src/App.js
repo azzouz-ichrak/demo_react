4068966b04171fe9b7161f1eebc1982ee003d954
chams <chamseddinechelly@gmail.com>
2021-04-17 09:41:47 +0100

    Event handler is a function

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import { useState } from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  // setTimeout(
-  //   () => setCounter(counter + 1),
-  //   1000
-  // )
-  const handleClick = () => {
-    console.log('clicked!!')
-  }
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
-  // console.log('rendering...', counter)
 
   return (
     <div>
       <h3>
         {counter}
       </h3>
-      <button onClick={() => setCounter(counter + 1)}>
+      <button onClick={increaseByOne}>
         Plus
+      </button>
+      <button onClick={setToZero}>
+        Zero
       </button>
     </div>
   )
