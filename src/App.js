f19538d5ba1ad956a3685cc42e11c1dff49079c0
samar8755 <samarbenkaouell@gmail.com>
2021-04-17 10:52:22 +0200

    event modification

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { useState } from 'react';
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
+ const increaseByOne =() => setCounter (counter+1)
+ const setToZero =() => setCounter(0)
 
   return (
     <div>
       <h3>
         {counter}
       </h3>
+      <button onClick ={increaseByOne}>
+      plus
+      </button>
+      <button onClick ={setToZero}>
+      zero
+      </button>
     </div>
   )
 }
